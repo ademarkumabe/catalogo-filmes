92848549618a8c5901415807b0b78c4d2453a7ec
ademar Kumabe <ademar.kumabe@hotmail.com>
2025-11-04 16:01:49 -0300

    Atividade Tranformar while para for

---
+++ src/RequisitosDoProjeto/TransformaWhileToFor.java
@@ -0,0 +1,20 @@
+package RequisitosDoProjeto;
+
+public class TransformaWhileToFor {
+
+        public static void main(String[] args) {
+
+            //int contador = 1;
+
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i);
+            }
+
+        /*
+            while(contador <= 10) {
+                System.out.println(contador);
+                contador++;
+        }
+        */
+    }
+}
